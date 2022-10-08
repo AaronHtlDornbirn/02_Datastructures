d3774897f976deba9f85dca1602a0b44e89d15ec
Martin Mueller <martin.ernst.mueller@gmail.com>
2022-10-08 09:44:54 +0200

    Add unit-Tests and Corrections

--- SingleLinkedListTest/UnitTest1.cs
+++ SingleLinkedListTest/UnitTest1.cs
@@ -19,5 +19,105 @@ namespace SingleLinkedListTest
             list.DeleteNode(2);
             Assert.AreEqual(list.Count(), 3);
         }
+
+        [Test]
+        public void IsertFirst_NotEmptyList_ElementAddedAtFirstPosOfList()
+        {
+            SingleLinkedList.SingleLinkedList list = new SingleLinkedList.SingleLinkedList();
+            list.InsertFirst("first");
+            list.InsertFirst("second");
+            var node = list.GetNode("second");
+            // muss ersten node sein -> next.data == "first"
+            Assert.IsNotNull(node);
+            Assert.AreEqual("second", node.data);
+            Assert.AreEqual(node.next.data, "first");
+        }
+
+
+        [Test]
+        public void IsertFirst_EmptyList_OnlyThisElementInList()
+        {
+            SingleLinkedList.SingleLinkedList list = new SingleLinkedList.SingleLinkedList();
+            list.InsertFirst("first");
+            var node = list.GetNode("first");
+            // muss ersten node sein -> next.data == "first"
+            Assert.IsNotNull(node);
+            Assert.AreEqual("first", node.data);
+            Assert.AreEqual(node.next, null);
+        }
+
+        [Test]
+        public void IsertLast_NotEmptyList_ElementAddedAtLastPosOfList()
+        {
+            SingleLinkedList.SingleLinkedList list = new SingleLinkedList.SingleLinkedList();
+            list.InsertLast("first");
+            list.InsertLast("second");
+            var node = list.GetNode("second");
+            // muss ersten node sein -> next.data == "first"
+            Assert.IsNotNull(node);
+            Assert.AreEqual("second", node.data);
+            Assert.AreEqual(node.next, null);
+        }
+
+
+        [Test]
+        public void IsertLast_EmptyList_OnlyThisElementInList()
+        {
+            SingleLinkedList.SingleLinkedList list = new SingleLinkedList.SingleLinkedList();
+            list.InsertFirst("first");
+            var node = list.GetNode("first");
+            // muss ersten node sein -> next.data == "first"
+            Assert.IsNotNull(node);
+            Assert.AreEqual("first", node.data);
+            Assert.AreEqual(node.next, null);
+        }
+
+        [Test]
+        public void IsertAt_NotEmptyList_ElementAddedAtLastPosOfList()
+        {
+            SingleLinkedList.SingleLinkedList list = new SingleLinkedList.SingleLinkedList();
+            list.InsertLast("first");
+            list.InsertLast("second");
+            list.InsertAt("middle", 1);
+            var node = list.GetNode("middle");
+            // muss ersten node sein -> next.data == "first"
+            Assert.IsNotNull(node);
+            // next node should be "second"
+            Assert.AreEqual("middle", node.data);
+            Assert.AreNotEqual(node.next, null);
+            Assert.AreEqual(node.next.data, "second");
+        }
+
+
+        [Test]
+        public void IsertLast_At_OnlyThisElementInList()
+        {
+            SingleLinkedList.SingleLinkedList list = new SingleLinkedList.SingleLinkedList();
+            list.InsertFirst("first");
+            var node = list.GetNode("first");
+            // muss ersten node sein -> next.data == "first"
+            Assert.IsNotNull(node);
+            Assert.AreEqual("first", node.data);
+            Assert.AreEqual(node.next, null);
+        }
+
+        [Test]
+        public void GetNode_EmptyList_ReturnsNull()
+        {
+            SingleLinkedList.SingleLinkedList list = new SingleLinkedList.SingleLinkedList();
+            var node = list.GetNode("first");
+            // throws Exectpoin -> bitte korrigieren
+        }
+
+        [Test]
+        public void GetNode_NodeExists_ReturnsNode()
+        {
+            SingleLinkedList.SingleLinkedList list = new SingleLinkedList.SingleLinkedList();
+            list.InsertFirst("first");
+            list.InsertFirst("second");
+            var node = list.GetNode("first");
+            Assert.AreEqual("first", node.data);
+
+        }
     }
 }
