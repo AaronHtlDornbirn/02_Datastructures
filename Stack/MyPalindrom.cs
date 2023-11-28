693113f5a10db3531cd6f8102ad6e5f4c4f3e18a
AaronHtlDornbirn <91245441+AaronHtlDornbirn@users.noreply.github.com>
2023-11-28 12:40:28 +0100

    finished

---
+++ Stack/MyPalindrom.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace Stack
+{
+    public class Palindrom
+    {
+        public static bool CheckPalindrom(string input)
+        {
+            Stack<char> PalindromStack = new Stack<char>(input);
+
+            foreach (char c in input)
+            {
+                if (PalindromStack.Pop() != c)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ StackTests/PalindromTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using Stack;
+
+namespace PalindromTests
+{
+    public class PalindromTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Palindrom_ANNA_True()
+        {
+            Assert.IsTrue(Palindrom.CheckPalindrom("ANNA"));
+        }
+
+        [Test]
+        public void Palindrom_OTTO_True()
+        {
+            Assert.IsTrue(Palindrom.CheckPalindrom("OTTO"));
+        }
+
+        [Test]
+        public void Palindrom_LAGERREGAL_True()
+        {
+            Assert.IsTrue(Palindrom.CheckPalindrom("LAGERREGAL"));
+        }
+
+        [Test]
+        public void Palindrom_AARON_False()
+        {
+            Assert.IsFalse(Palindrom.CheckPalindrom("AARON"));
+        }
+    }
+}
